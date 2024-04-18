11ec00fc0e7f27ecaa7735de90171ed1b57e4df1
Pelayo <120382226+pelayord76@users.noreply.github.com>
2024-04-18 15:51:00 +0000

    simplificada la base de datos de usuarios, lo proximo terminar los cruds de recaudaciones y de clientes, con sus vistas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,11 @@
 INSERT INTO usuario (nombre, email, contrasenia) VALUES('admin', 'admin@email.com', '$2a$12$YORr2teNaSVSof5LPbkCIuk5y4549Wp4ZYdW.NYqrJNFk2FWW.e5K');
-INSERT INTO usuario (nombre, email, contrasenia) VALUES('raso', 'usuario2@email.com', '$2a$12$k5Ia/9oKC.WPvzfF8b73meex/bF5C5sda0h4CSkXBrcGGSWI9jAW.');
-INSERT INTO usuario (nombre, email, contrasenia) VALUES('pepe', 'pepe@email.com', '$2a$12$E9AI9DOVpvhbAT5rWLAnUuJ4vnzpM3r4boiTOu2Nfxwq9orpWi1Pe');
-INSERT INTO usuario (nombre, email, contrasenia) VALUES('pelayo', 'pelayo@email.com', '$2a$12$aI38mqhCOlBbZQqNzs8pMu1DgONm4M.McTvHfr3LFuJTE//52SJAS');
+INSERT INTO usuario (nombre, email, contrasenia) VALUES('raso', 'raso@email.com', '$2a$12$k5Ia/9oKC.WPvzfF8b73meex/bF5C5sda0h4CSkXBrcGGSWI9jAW.');
 
 INSERT INTO cliente (local, duenio, telefono, fecha_vencimiento_contrato) VALUES('Long island', 'dueño1', 000000000,'2024-12-31');
-INSERT INTO cliente (local, duenio, telefono, fecha_vencimiento_contrato) VALUES('Bar2', 'dueño2', 111111111,'2025-12-31');
-INSERT INTO cliente (local, duenio, telefono, fecha_vencimiento_contrato) VALUES('Bar3', 'dueño3', 222222222,'2023-12-31');
-INSERT INTO cliente (local, duenio, telefono, fecha_vencimiento_contrato) VALUES('Bar4', 'dueño4', 333333333,'2025-12-31');
-INSERT INTO cliente (local, duenio, telefono, fecha_vencimiento_contrato) VALUES('bar sin maquina', 'dueño4', 444444444,'2027-12-31');
+INSERT INTO cliente (local, duenio, telefono, fecha_vencimiento_contrato) VALUES('B12', 'dueño2', 111111111,'2025-12-31');
+INSERT INTO cliente (local, duenio, telefono, fecha_vencimiento_contrato) VALUES('La Espuela', 'dueño3', 222222222,'2023-12-31');
+INSERT INTO cliente (local, duenio, telefono, fecha_vencimiento_contrato) VALUES('La Casuca', 'dueño4', 333333333,'2025-12-31');
+INSERT INTO cliente (local, duenio, telefono, fecha_vencimiento_contrato) VALUES('Copas (sin maquina)', 'dueño4', 444444444,'2027-12-31');
 
 
 INSERT INTO maquina (nombre, fecha_vencimiento_licencia, almacenada, FK_CLIENTE) VALUES('Neopolis', '2024-12-31', 0, 1);
@@ -18,10 +16,13 @@ INSERT INTO maquina (nombre, fecha_vencimiento_licencia, almacenada, FK_CLIENTE)
 
 INSERT INTO tiene (usuario_id, maquina_id) VALUES(1, 1);
 INSERT INTO tiene (usuario_id, maquina_id) VALUES(1, 2);
+INSERT INTO tiene (usuario_id, maquina_id) VALUES(1, 3);
+INSERT INTO tiene (usuario_id, maquina_id) VALUES(1, 4);
 INSERT INTO tiene (usuario_id, maquina_id) VALUES(2, 1);
 INSERT INTO tiene (usuario_id, maquina_id) VALUES(2, 2);
-INSERT INTO tiene (usuario_id, maquina_id) VALUES(3, 3);
-INSERT INTO tiene (usuario_id, maquina_id) VALUES(3, 4);
+INSERT INTO tiene (usuario_id, maquina_id) VALUES(2, 3);
+INSERT INTO tiene (usuario_id, maquina_id) VALUES(2, 4);
+
 
 INSERT INTO recaudacion (cantidad_recaudada, porcentaje_juego, fecha, FK_MAQUINA) VALUES(1000.0, 0.35, '2023-12-06', 1);
 INSERT INTO recaudacion (cantidad_recaudada, porcentaje_juego, fecha, FK_MAQUINA) VALUES(1500.0, 0.77, '2023-12-06', 2);
