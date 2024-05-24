5290d09d494d18595ae8276b891ad0fd0826c54b
Pelayo R. Diaz <pelayo.rodriguez@externos.seresco.es>
2024-05-24 12:17:00 +0200

    servicio recaudacion

--- src/main/java/com/spring/start/mapper/RecaudacionMapper.java
+++ src/main/java/com/spring/start/mapper/RecaudacionMapper.java
@@ -1,8 +1,23 @@
 package com.spring.start.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
+
+import com.spring.start.dto.recaudacion.RecaudacionRequestDto;
+import com.spring.start.dto.recaudacion.RecaudacionResponseDto;
+import com.spring.start.entity.Recaudacion;
 
 @Mapper(componentModel = "spring")
 public interface RecaudacionMapper {
 
+	public RecaudacionResponseDto mapToRecaudacionResponseDto(Recaudacion recaudacion);
+
+	public List<RecaudacionResponseDto> mapToRecaudacionResponseDto(List<Recaudacion> recaudacions);
+
+	public Recaudacion mapRecaudacionRequestToRecaudacion(RecaudacionRequestDto dto);
+
+	public Recaudacion mapRecaudacionRequestToRecaudacion(Long id, RecaudacionRequestDto dto);
+
+	public RecaudacionResponseDto mapRecaudacionRequestToRecaudacionResponse(RecaudacionRequestDto dto);
 }
--- src/main/java/com/spring/start/service/impl/RecaudacionServiceImpl.java
+++ src/main/java/com/spring/start/service/impl/RecaudacionServiceImpl.java
@@ -1,41 +1,62 @@
 package com.spring.start.service.impl;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.spring.start.dto.recaudacion.RecaudacionRequestDto;
 import com.spring.start.dto.recaudacion.RecaudacionResponseDto;
+import com.spring.start.entity.Recaudacion;
+import com.spring.start.mapper.RecaudacionMapper;
+import com.spring.start.repository.RecaudacionRepository;
 import com.spring.start.service.RecaudacionService;
 
 public class RecaudacionServiceImpl implements RecaudacionService {
 
+	@Autowired
+	private RecaudacionRepository recaudacionRepository;
+
+	@Autowired
+	private RecaudacionMapper recaudacionMapper;
+
 	@Override
 	public RecaudacionResponseDto findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Recaudacion> recaudacionOptional = recaudacionRepository.findById(id);
+		if (recaudacionOptional.isEmpty()) {
+			throw new IllegalArgumentException("Esa recaudacion no existe");
+		}
+		return recaudacionMapper.mapToRecaudacionResponseDto(recaudacionOptional.get());
 	}
 
 	@Override
 	public List<RecaudacionResponseDto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Recaudacion> recaudaciones = recaudacionRepository.findAll();
+		return recaudacionMapper.mapToRecaudacionResponseDto(recaudaciones);
 	}
 
 	@Override
 	public RecaudacionResponseDto add(RecaudacionRequestDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		recaudacionRepository.save(recaudacionMapper.mapRecaudacionRequestToRecaudacion(dto));
+		return recaudacionMapper.mapRecaudacionRequestToRecaudacionResponse(dto);
 	}
 
 	@Override
 	public RecaudacionResponseDto update(Long id, RecaudacionRequestDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Recaudacion> recaudacionOptional = recaudacionRepository.findById(id);
+		if (recaudacionOptional.isEmpty()) {
+			throw new IllegalArgumentException("Esa recaudacion no existe");
+		}
+		Recaudacion recaudacion = recaudacionMapper.mapRecaudacionRequestToRecaudacion(id, dto);
+		recaudacionRepository.save(recaudacion);
+		return recaudacionMapper.mapRecaudacionRequestToRecaudacionResponse(dto);
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
-
+		if (!recaudacionRepository.findById(id).isPresent()) {
+			throw new IllegalArgumentException("No existe esa recaudacion");
+		}
+		recaudacionRepository.deleteById(id);
 	}
-
 }
