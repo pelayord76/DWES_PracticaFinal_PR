3a77f3325c123a247d7bcf8babdeb8b207cb82e1
Pelayo R. Diaz <pelayo.rodriguez@externos.seresco.es>
2024-05-24 12:26:48 +0200

    usuario service

--- src/main/java/com/spring/start/mapper/UsuarioMapper.java
+++ src/main/java/com/spring/start/mapper/UsuarioMapper.java
@@ -1,7 +1,23 @@
 package com.spring.start.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
+
+import com.spring.start.dto.usuario.UsuarioRequestDto;
+import com.spring.start.dto.usuario.UsuarioResponseDto;
+import com.spring.start.entity.Usuario;
 
 @Mapper(componentModel = "spring")
 public interface UsuarioMapper {
+
+	public UsuarioResponseDto mapToUsuarioResponseDto(Usuario usuario);
+
+	public List<UsuarioResponseDto> mapToUsuarioResponseDto(List<Usuario> usuarios);
+
+	public Usuario mapUsuarioRequestToUsuario(UsuarioRequestDto dto);
+
+	public Usuario mapUsuarioRequestToUsuario(Long id, UsuarioRequestDto dto);
+
+	public UsuarioResponseDto mapUsuarioRequestToUsuarioResponse(UsuarioRequestDto dto);
 }
--- src/main/java/com/spring/start/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/spring/start/service/impl/UsuarioServiceImpl.java
@@ -1,41 +1,63 @@
 package com.spring.start.service.impl;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.spring.start.dto.usuario.UsuarioRequestDto;
 import com.spring.start.dto.usuario.UsuarioResponseDto;
+import com.spring.start.entity.Usuario;
+import com.spring.start.mapper.UsuarioMapper;
+import com.spring.start.repository.UsuarioRepository;
 import com.spring.start.service.UsuarioService;
 
 public class UsuarioServiceImpl implements UsuarioService {
 
+	@Autowired
+	UsuarioRepository usuarioRepository;
+
+	@Autowired
+	UsuarioMapper usuarioMapper;
+
 	@Override
 	public UsuarioResponseDto findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+		if (usuarioOptional.isEmpty()) {
+			throw new IllegalArgumentException("No existe ese usuario");
+		}
+		return usuarioMapper.mapToUsuarioResponseDto(usuarioOptional.get());
 	}
 
 	@Override
 	public List<UsuarioResponseDto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		return usuarioMapper.mapToUsuarioResponseDto(usuarios);
 	}
 
 	@Override
 	public UsuarioResponseDto add(UsuarioRequestDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		usuarioRepository.save(usuarioMapper.mapUsuarioRequestToUsuario(dto));
+		return usuarioMapper.mapUsuarioRequestToUsuarioResponse(dto);
 	}
 
 	@Override
 	public UsuarioResponseDto update(Long id, UsuarioRequestDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+		if (usuarioOptional.isEmpty()) {
+			throw new IllegalArgumentException("No existe ese usuario");
+		}
+		Usuario usuario = usuarioMapper.mapUsuarioRequestToUsuario(id, dto);
+		usuarioRepository.save(usuario);
+		return usuarioMapper.mapUsuarioRequestToUsuarioResponse(dto);
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
-
+		if (!usuarioRepository.findById(id).isPresent()) {
+			throw new IllegalArgumentException("No existe ese usuario");
+		}
+		usuarioRepository.deleteById(id);
 	}
 
 }
